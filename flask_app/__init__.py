a416bc9b82fc3085f11f6760e5774aca490950c3
unknown <pokem@naver.com>
2022-11-02 21:57:34 +0900

    index.html added

--- flask_app/__init__.py
+++ flask_app/__init__.py
@@ -8,9 +8,10 @@ def create_app(config=None):
         app.config.update(config)
     
     from flask_app.api import api_bp
+    from flask_app.views.main_views import main_bp
     
+    app.register_blueprint(main_bp)
     app.register_blueprint(api_bp, url_prefix='/api')
-    
     
     return app
         
